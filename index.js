4f8a640cda2915923fa42303e738dc12b8f0dfcc
Nagano Aya <x18086@osaka-geidai.ac.jp>
2019-09-30 13:10:07 +0900

    真ん中に行きません！

---
+++ index.js
@@ -0,0 +1,55 @@
+$(function () {
+	
+	var page = 0;
+
+	
+	var lastPage = parseInt($("#slide img").length - 1);
+
+	
+	$("#slide img").css("display", "none");
+
+	
+	$("#slide img").eq(page).css("display", "block");
+
+	
+	function changePage() {
+		$("#slide img").fadeOut(1000);
+		$("#slide img").eq(page).fadeIn(1000);
+	};
+
+	
+	var Timer;
+
+	function startTimer() {
+		Timer = setInterval(function () {
+			if (page === lastPage) {
+				page = 0;
+				changePage();
+			} else {
+				page++;
+				changePage();
+			};
+		}, 5000);
+	}
+	
+	function stopTimer() {
+		clearInterval(Timer);
+	}
+
+	
+	startTimer();
+});
+
+jQuery(function () {
+	jQuery(window).scroll(function () {
+		jQuery('.fadein').each(function () {
+			var elemPos = jQuery(this).offset().top;
+			var scroll = jQuery(window).scrollTop();
+			var windowHeight = jQuery(window).height();
+			if (scroll > elemPos - windowHeight + 100) {
+				jQuery(this).addClass('scrollin');
+			}
+		});
+	});
+	jQuery(window).scroll();
+});
